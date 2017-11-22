70cff44b0c3df8cae490c983f9e75e507f7b4cf3
Wabri <gabriele.puliti@gmail.com>
2017-11-22 16:04:12 +0100

    created test get all employee when there is one Employee

--- exercise-core/src/main/java/com/examples/attsw/exercise/core/model/Employee.java
+++ exercise-core/src/main/java/com/examples/attsw/exercise/core/model/Employee.java
@@ -2,4 +2,23 @@ package com.examples.attsw.exercise.core.model;
 
 public class Employee {
 
+	private String name;
+
+	public Employee(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
+
 }
--- exercise-core/src/test/java/com/examples/attsw/exercise/core/controller/EmployeeControllerTest.java
+++ exercise-core/src/test/java/com/examples/attsw/exercise/core/controller/EmployeeControllerTest.java
@@ -2,26 +2,45 @@ package com.examples.attsw.exercise.core.controller;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import com.examples.attsw.exercise.core.model.Employee;
 import com.examples.attsw.exercise.core.service.IEmployeeService;
 
 public class EmployeeControllerTest {
 
 	public EmployeeController employeeController;
+	private List<Employee> employees;
+	private IEmployeeService iEmployeeService;
 
 	@Before
 	public void setUp() throws Exception {
-		IEmployeeService iEmployeeService = Mockito.mock(IEmployeeService.class);
+		iEmployeeService = Mockito.mock(IEmployeeService.class);
 		employeeController = new EmployeeController(iEmployeeService);
+		employees = new LinkedList<Employee>();
+		Mockito.when(iEmployeeService.getEmployees()).thenReturn(employees);
 	}
 
 	@Test
 	public void testGetAllEmployeeWhenThereAreNoEmployee() {
+		assertGetAllEmployee("");
+	}
+
+	@Test
+	public void testGetAllEmployeeWhenThereIsOneEmployee() {
+		Employee employee = new Employee("nameTest");
+		employees.add(employee);
+		assertGetAllEmployee(employee.getName());
+	}
+
+	private void assertGetAllEmployee(String expected) {
 		String allEmployee = employeeController.getAllEmployee();
-		assertEquals("", allEmployee);
+		assertEquals(expected, allEmployee);
 	}
 
 }
